9e84a87ab3922697d4c26a988bd0e89ca8bfc508
Husain Al Yusuf <halyusuf@email.arizona.edu>
2026-08-24 05:38:28 -0700

    update SE+UB command for acdc

--- networks/vit_seg_configs.py
+++ networks/vit_seg_configs.py
@@ -15,7 +15,7 @@ def get_b16_config():
     config.classifier = 'seg'
     config.representation_size = None
     config.resnet_pretrained_path = None
-    config.pretrained_path = '../model/vit_checkpoint/imagenet21k/imagenet21k_R50+ViT-B_16.npz'
+    config.pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/imagenet21k_R50+ViT-B_16.npz'
     config.patch_size = 16
 
     config.decoder_channels = (256, 128, 64, 16)
@@ -48,9 +48,9 @@ def get_r50_b16_config():
     config.resnet.width_factor = 1
 
     config.classifier = 'seg'
-    # config.pretrained_path = '../model/vit_checkpoint/imagenet21k/R50+ViT-B_16.npz'
+    # config.pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/R50+ViT-B_16.npz'
     # config.pretrained_path = './model/vit_checkpoint/imagenet21k/imagenet21k_R50+ViT-B_16.npz'
-    config.pretrained_path = '../model/vit_checkpoint/imagenet21k/R50+ViT-B_16.npz'
+    config.pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/R50+ViT-B_16.npz'
     config.decoder_channels = (256, 128, 64, 16)
     config.skip_channels = [512, 256, 64, 16]
     config.n_classes = 2
@@ -64,7 +64,7 @@ def get_b32_config():
     """Returns the ViT-B/32 configuration."""
     config = get_b16_config()
     config.patches.size = (32, 32)
-    config.pretrained_path = '../model/vit_checkpoint/imagenet21k/ViT-B_32.npz'
+    config.pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/ViT-B_32.npz'
     return config
 
 
@@ -84,7 +84,7 @@ def get_l16_config():
     # custom
     config.classifier = 'seg'
     config.resnet_pretrained_path = None
-    config.pretrained_path = '../model/vit_checkpoint/imagenet21k/ViT-L_16.npz'
+    config.pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/ViT-L_16.npz'
     config.decoder_channels = (256, 128, 64, 16)
     config.n_classes = 2
     config.activation = 'softmax'
@@ -100,7 +100,7 @@ def get_r50_l16_config():
     config.resnet.width_factor = 1
 
     config.classifier = 'seg'
-    config.resnet_pretrained_path = '../model/vit_checkpoint/imagenet21k/R50+ViT-B_16.npz'
+    config.resnet_pretrained_path = '/data/halyusuf/data/pretrained_backbones/vit_imagenet21k/R50+ViT-B_16.npz'
     config.decoder_channels = (256, 128, 64, 16)
     config.skip_channels = [512, 256, 64, 16]
     config.n_classes = 2
